c37126fb4681b372e57e203192c0a7f6c43d79c9
Kimserey Lam <kimserey.lam@gmail.com>
2016-10-06 09:16:20 +0100

    folders

--- FileFolders/Resources/Resource.designer.cs
+++ FileFolders/Resources/Resource.designer.cs
@@ -1426,8 +1426,8 @@ namespace FileFolders
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f0c0056
-			public const int action0 = 2131492950;
+			// aapt resource value: 0x7f0c0059
+			public const int action0 = 2131492953;
 			
 			// aapt resource value: 0x7f0c0041
 			public const int action_bar = 2131492929;
@@ -1453,8 +1453,8 @@ namespace FileFolders
 			// aapt resource value: 0x7f0c0042
 			public const int action_context_bar = 2131492930;
 			
-			// aapt resource value: 0x7f0c005a
-			public const int action_divider = 2131492954;
+			// aapt resource value: 0x7f0c005d
+			public const int action_divider = 2131492957;
 			
 			// aapt resource value: 0x7f0c0002
 			public const int action_menu_divider = 2131492866;
@@ -1486,14 +1486,14 @@ namespace FileFolders
 			// aapt resource value: 0x7f0c002b
 			public const int buttonPanel = 2131492907;
 			
-			// aapt resource value: 0x7f0c0057
-			public const int cancel_action = 2131492951;
+			// aapt resource value: 0x7f0c005a
+			public const int cancel_action = 2131492954;
 			
 			// aapt resource value: 0x7f0c0039
 			public const int checkbox = 2131492921;
 			
-			// aapt resource value: 0x7f0c005d
-			public const int chronometer = 2131492957;
+			// aapt resource value: 0x7f0c0060
+			public const int chronometer = 2131492960;
 			
 			// aapt resource value: 0x7f0c001c
 			public const int collapseActionView = 2131492892;
@@ -1516,8 +1516,8 @@ namespace FileFolders
 			// aapt resource value: 0x7f0c000c
 			public const int disableHome = 2131492876;
 			
-			// aapt resource value: 0x7f0c0055
-			public const int documentsDir = 2131492949;
+			// aapt resource value: 0x7f0c0058
+			public const int documentsDir = 2131492952;
 			
 			// aapt resource value: 0x7f0c0043
 			public const int edit_query = 2131492931;
@@ -1525,8 +1525,8 @@ namespace FileFolders
 			// aapt resource value: 0x7f0c0019
 			public const int end = 2131492889;
 			
-			// aapt resource value: 0x7f0c0062
-			public const int end_padder = 2131492962;
+			// aapt resource value: 0x7f0c0065
+			public const int end_padder = 2131492965;
 			
 			// aapt resource value: 0x7f0c0025
 			public const int expand_activities_button = 2131492901;
@@ -1534,8 +1534,8 @@ namespace FileFolders
 			// aapt resource value: 0x7f0c0038
 			public const int expanded_menu = 2131492920;
 			
-			// aapt resource value: 0x7f0c0051
-			public const int filesDir = 2131492945;
+			// aapt resource value: 0x7f0c0052
+			public const int filesDir = 2131492946;
 			
 			// aapt resource value: 0x7f0c0004
 			public const int home = 2131492868;
@@ -1552,17 +1552,26 @@ namespace FileFolders
 			// aapt resource value: 0x7f0c0026
 			public const int image = 2131492902;
 			
-			// aapt resource value: 0x7f0c0061
-			public const int info = 2131492961;
+			// aapt resource value: 0x7f0c0064
+			public const int info = 2131492964;
 			
-			// aapt resource value: 0x7f0c0052
-			public const int label2 = 2131492946;
+			// aapt resource value: 0x7f0c0054
+			public const int label2 = 2131492948;
 			
-			// aapt resource value: 0x7f0c005b
-			public const int line1 = 2131492955;
+			// aapt resource value: 0x7f0c005e
+			public const int line1 = 2131492958;
 			
-			// aapt resource value: 0x7f0c005f
-			public const int line3 = 2131492959;
+			// aapt resource value: 0x7f0c0062
+			public const int line3 = 2131492962;
+			
+			// aapt resource value: 0x7f0c0050
+			public const int linearLayout1 = 2131492944;
+			
+			// aapt resource value: 0x7f0c0053
+			public const int linearLayout2 = 2131492947;
+			
+			// aapt resource value: 0x7f0c0056
+			public const int linearLayout3 = 2131492950;
 			
 			// aapt resource value: 0x7f0c0009
 			public const int listMode = 2131492873;
@@ -1570,8 +1579,8 @@ namespace FileFolders
 			// aapt resource value: 0x7f0c0028
 			public const int list_item = 2131492904;
 			
-			// aapt resource value: 0x7f0c0059
-			public const int media_actions = 2131492953;
+			// aapt resource value: 0x7f0c005c
+			public const int media_actions = 2131492956;
 			
 			// aapt resource value: 0x7f0c001a
 			public const int middle = 2131492890;
@@ -1591,8 +1600,8 @@ namespace FileFolders
 			// aapt resource value: 0x7f0c002d
 			public const int parentPanel = 2131492909;
 			
-			// aapt resource value: 0x7f0c0053
-			public const int picturesDir = 2131492947;
+			// aapt resource value: 0x7f0c0055
+			public const int picturesDir = 2131492949;
 			
 			// aapt resource value: 0x7f0c0005
 			public const int progress_circular = 2131492869;
@@ -1675,8 +1684,8 @@ namespace FileFolders
 			// aapt resource value: 0x7f0c0017
 			public const int src_over = 2131492887;
 			
-			// aapt resource value: 0x7f0c0058
-			public const int status_bar_latest_event_content = 2131492952;
+			// aapt resource value: 0x7f0c005b
+			public const int status_bar_latest_event_content = 2131492955;
 			
 			// aapt resource value: 0x7f0c004c
 			public const int submit_area = 2131492940;
@@ -1684,23 +1693,23 @@ namespace FileFolders
 			// aapt resource value: 0x7f0c000b
 			public const int tabMode = 2131492875;
 			
-			// aapt resource value: 0x7f0c0060
-			public const int text = 2131492960;
+			// aapt resource value: 0x7f0c0063
+			public const int text = 2131492963;
 			
-			// aapt resource value: 0x7f0c005e
-			public const int text2 = 2131492958;
+			// aapt resource value: 0x7f0c0061
+			public const int text2 = 2131492961;
 			
 			// aapt resource value: 0x7f0c0034
 			public const int textSpacerNoButtons = 2131492916;
 			
-			// aapt resource value: 0x7f0c0050
-			public const int textView1 = 2131492944;
+			// aapt resource value: 0x7f0c0051
+			public const int textView1 = 2131492945;
 			
-			// aapt resource value: 0x7f0c0054
-			public const int textView2 = 2131492948;
+			// aapt resource value: 0x7f0c0057
+			public const int textView2 = 2131492951;
 			
-			// aapt resource value: 0x7f0c005c
-			public const int time = 2131492956;
+			// aapt resource value: 0x7f0c005f
+			public const int time = 2131492959;
 			
 			// aapt resource value: 0x7f0c002a
 			public const int title = 2131492906;
